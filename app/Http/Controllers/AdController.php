edbe61ef69fb8b6f00de85f1e2de87e638198307
Claudia <claudia.iacob@hotmail.com>
2022-12-16 21:13:39 +0100

    Creacion vista create

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Termwind\Components\Dd;
 
 class AdController extends Controller
 {
-    //
+
+public function __construct(){
+    $this->middleware('auth');
+}
+
+public function create (){
+    return view('ad.create');
+}
 }
