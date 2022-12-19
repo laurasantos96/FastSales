7be587c3102f9a245ba3b8917ff61367c779e90d
Claudia <claudia.iacob@hotmail.com>
2022-12-19 21:53:51 +0100

    Modificado vista card welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
                     <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title"> {{$ad->title}}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{{$ad->price}}</h6>
+                        <h6 class="card-subtitle mb-2 text-muted">€ {{$ad->price}}</h6>
                         <p class="card-text"> {{$ad->body}}</p>
                         <div class="card-subtitle mb-2">
                             <strong><a href="#">#{{$ad->category->name}}</a></strong>
