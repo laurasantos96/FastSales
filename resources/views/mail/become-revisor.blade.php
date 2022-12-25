a6cf413d33132a3666593fbe84d4415792508a03
Claudia <claudia.iacob@hotmail.com>
2022-12-25 20:18:25 +0100

    Cambiado become-revisor.blade

--- resources/views/mail/become-revisor.blade.php
+++ resources/views/mail/become-revisor.blade.php
@@ -1,10 +1,22 @@
-<x-layout>
-  <div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+    <div>
     <h1>Alguien quiere trabajar con nosotros</h1>
     <h2>Sus Datos</h2>
     <p><b>Nombre:</b>{{$user->name}} </p>
     <p><b>Email:</b>{{$user->email}} </p>
     <p>Si quieres ser parte de nuestro equipo pulsa aquí</p>
-    <a href="{{route('revisor.make',$user)}} ">Acceptar solicitud</a>
+    <a href="{{route('revisor.make',$user)}} ">Aceptar solicitud</a>
   </div>
-</x-layout>
+</body>
+</html>
+
+
+
