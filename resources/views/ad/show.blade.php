278ecf9558b8dec1759456b072f9c6679bc9eda8
Claudia <claudia.iacob@hotmail.com>
2022-12-28 20:35:37 +0100

    User 5 acabado

--- resources/views/ad/show.blade.php
+++ resources/views/ad/show.blade.php
@@ -1,28 +1,29 @@
 <x-layout>
   <div class="row justify-content-center w-100">
     <div class="col-md-8">
-    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="{{asset('img/carruselmoviles.jpg')}}" class="d-block" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="{{asset('img/carruseldeporte.jpg')}}" class="d-block" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="{{asset('img/carruselcoche.jpg')}}" class="d-block" alt="...">
-        </div>
+    <div id="adImage" class="carousel slide carousel-fade" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        @for ($i = 0; $i < $ad->image()->count(); $i++)
+        <button type="button" data-bs-target="#adImage" data-bs-slide-to="{{$i}}" class="@if ($i==0) active @endif" aria-current="true" aria-label="Slide {{$i + 1}}"></button>
+        @endfor
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+      <div class="carousel-inner">
+        @foreach ($ad->image as $image)
+           <div class="carousel-item @if ($loop->first) active @endif">
+          <img src="{{Storage::url($image()->path)}}" class="d-block" alt="...">
+        </div>
+        @endforeach
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#adImage" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
       </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+      <button class="carousel-control-next" type="button" data-bs-target="#adImage" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
       </button>
     </div>
-
+  </div>
     <div class="col-12 col-md-6">
       <div><b>{{__('Titulo:')}}</b>{{$ad->title}}</div>
       <div><b>{{__('Precio:')}}</b>{{$ad->price}}</div>
@@ -33,5 +34,5 @@
       <div><a href="#" class="btn btn-success">{{__('Comprar')}}</a></div>
     </div>
   </div>
-</div>
+
 </x-layout>
