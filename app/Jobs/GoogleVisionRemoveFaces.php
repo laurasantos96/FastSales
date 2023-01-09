2b9e6b504942688ba450dc4a68089dcd720bfa73
helena rodrigues <helena19.rodrigues@gmail.com>
2023-01-09 19:58:08 +0000

    creacion de remove face

---
+++ app/Jobs/GoogleVisionRemoveFaces.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use App\Models\Image;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
+
+class GoogleVisionRemoveFaces implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    private $image_id;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($image_id)
+    {
+        $this->image_id = $image_id;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $i = Image::find($this->image_id);
+        if (!$i) {
+            return;
+        }
+        $srcPath = storage_path('/app/public' . $i->path);
+        $image = file_get_contents($srcPath);
+        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google_credentials.json'));
+        $imageAnnotator = new ImageAnnotatorClient();
+        $response = $imageAnnotator->faceDetection($image);
+        $imageAnnotator->close();
+        $faces = $response->getFaceAnnotations();
+
+        foreach ($faces as $face) {
+            $vertices = $face->getBoundingPoly()->getVertices();
+            echo "face\n";
+            foreach ($vertices as $vertex) {
+                echo $vertex->getX() . "," . $vertex->getY() . "\n";
+            }
+        }
+    }
+}
