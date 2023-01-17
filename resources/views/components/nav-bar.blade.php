e46573e5b740e93917de2077fe7c1ab982c3d810
Claudia <claudia.iacob@hotmail.com>
2023-01-17 12:25:03 +0100

    Carritas

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -60,6 +60,7 @@
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle nav_letra" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
+                                {{-- 😊 --}}
                             </a>
                             
                           
